700af0d71b73d20db1b6d84118f391c43962ef0c
goscha01 <sayapingeorge@gmail.com>
2025-11-26 22:23:20 -0500

    Bump Android versionCode to 14

--- app.config.js
+++ app.config.js
@@ -49,7 +49,7 @@ export default {
         backgroundColor: "#F2C31B"
       },
       package: "com.proofpix.app",
-      versionCode: 13,
+      versionCode: 14,
       permissions: [
         "CAMERA",
         "WRITE_EXTERNAL_STORAGE",
