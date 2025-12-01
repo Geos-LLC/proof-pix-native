16180a1a166f39654f256014946030ea14da35b7
goscha01 <sayapingeorge@gmail.com>
2025-11-30 21:20:57 -0500

    updated build to 10

--- app.config.js
+++ app.config.js
@@ -26,7 +26,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.proofpix.app",
-      buildNumber: "5",
+      buildNumber: "10",
       googleServicesFile: "./GoogleService-Info.plist",
       requireFullScreen: false,
       infoPlist: {
