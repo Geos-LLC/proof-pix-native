e144026238be9c23ed154c6f7779e9cc9a9af9d5
goscha01 <sayapingeorge@gmail.com>
2025-12-07 17:09:15 -0500

    updated build

--- app.config.js
+++ app.config.js
@@ -49,7 +49,7 @@ export default {
         backgroundColor: "#F2C31B"
       },
       package: "com.proofpix.app",
-      versionCode: 20,
+      versionCode: 21,
       permissions: [
         "CAMERA",
         "WRITE_EXTERNAL_STORAGE",
