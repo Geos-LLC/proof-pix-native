99b7205513d3f97462b031f8d4fc2230ce60f733
goscha01 <sayapingeorge@gmail.com>
2026-01-27 12:35:26 -0500

    project id change for eas build

--- app.config.js
+++ app.config.js
@@ -134,7 +134,7 @@ export default {
     ],
     extra: {
       eas: {
-        projectId: "3e7ed884-bb3a-4191-bcb0-188386d7e977"
+        projectId: "c65badb3-ddbc-4bb8-9de5-fab32a427f16"
       },
       // Environment variables accessible in your app
       googleServiceAccountPath: process.env.GOOGLE_SERVICE_ACCOUNT_PATH,
