ae4bf817942e0fbd8ae731a92e2b8f18d5b47444
Weblord <68680206+bopmack@users.noreply.github.com>
2026-04-03 02:29:52 -0400

    version changes

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: process.env.APP_NAME || "ProofPix",
     slug: "proof-pix-native",
-    version: process.env.VERSION || "1.5.1",
+    version: process.env.VERSION || "1.5.2",
     runtimeVersion: {
       policy: "appVersion"
     },
@@ -27,7 +27,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.proofpix.app",
-      buildNumber: "31",
+      buildNumber: "32",
       googleServicesFile: "./GoogleService-Info.plist",
       requireFullScreen: false,
       infoPlist: {
@@ -68,7 +68,7 @@ export default {
         backgroundColor: "#F2C31B"
       },
       package: "com.proofpix.app",
-      versionCode: 37,
+      versionCode: 38,
       permissions: [
         "CAMERA",
         "WRITE_EXTERNAL_STORAGE",
